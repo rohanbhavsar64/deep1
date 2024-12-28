c178f7c0d5a606df2a82d32a58a8352f9aead2c0
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-28 19:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 # Load the MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# Normalize the images to the range [0, 1]
-x_train = x_train.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
-
-# Reshape the data to fit the model input
-x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
-x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
-
-# Convert labels to one-hot encoding
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=5, batch_size=60, validation_split=0.2)
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print(f'Test accuracy: {test_acc}')
+model = load_model('my_model.h5')
 st.title('Deep Learning Model Prediction')
 st.write('Upload an image to get predictions from the model.')
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
