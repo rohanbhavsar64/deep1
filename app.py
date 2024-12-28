803fd822ca271e2d2eed0804eb8d85b6f8cc473d
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-28 19:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ model.add(layers.Dense(10, activation='softmax'))
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=2, batch_size=64, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=5, batch_size=60, validation_split=0.2)
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f'Test accuracy: {test_acc}')
 st.title('Deep Learning Model Prediction')
