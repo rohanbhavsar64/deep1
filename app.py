4ab1bd4d64f89f614207c0399d3f6ddadca49dad
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-20 11:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 import time
-import warnings
 import torch
 from faster_whisper import WhisperModel
 import streamlit as st
 
 class WhisperTranscriber:
     def __init__(self, model_size="large-v2"):
-        warnings.filterwarnings("ignore")
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Using device: {self.device}")
         # Measure the time taken to load the model
@@ -52,42 +50,3 @@ if __name__ == "__main__":
             st.markdown(f"Transcription: {result['transcription']}")
             print(f"Provided Language: {result['language']}")
             print(f"Total time for processing: {total_time:.2f} seconds.")
-    
-    return model, device
-
-def transcribe_and_translate(model, audio_file):
-    """
-    Transcribe and translate the audio file using the provided model.
-    Returns the transcription text and detected language.
-    """
-    start_time = time.time()
-    print("Transcribing and translating audio...")
-
-    # Perform transcription and translation0
-    segments, info = model.transcribe(audio_file, beam_size=2, task="translate")
-    transcription = "".join([segment.text for segment in segments])
-    
-    # Measure the time taken
-    process_time = time.time() - start_time
-    print(f"Process completed in {process_time:.2f} seconds.")
-    
-    return {"transcription": transcription, "language": info.language}
-    
-if __name__ == "__main__":
-    # Specify the audio file
-    audio_file = "/content/WIN_20240730_19_25_14_Pro.mp4"
-    model_size = "large"
-
-    # Initialize the model
-    model, device = initialize_model(model_size=model_size)
-    
-    # Measure total processing time
-    total_start_time = time.time()
-    result = transcribe_and_translate(model, audio_file)
-    total_time = time.time() - total_start_time
-    
-    # Display the results
-    print("\n--- Speech-to-Text Results ---")
-    print(f"Transcription: {result['transcription']}")
-    print(f"Detected Language: {result['language']}")
-    print(f"Total time for processing: {total_time:.2f} seconds.")
